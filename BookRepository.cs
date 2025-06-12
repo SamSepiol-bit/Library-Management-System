f9e0d288b6c8424532f218500018cd9a093e1982
SamSepiol-bit <icbtworkfor@gmail.com>
2025-06-12 14:14:24 +0530

    Add files via upload

---
+++ BookRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryApp
+{
+    public class BookRepository
+    {
+    }
+}
---
+++ Librarian.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryApp
+{
+    public class Librarian
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
