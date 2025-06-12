bda7aa7af572b9b15483eff9345fcd7386a5052b
SamSepiol-bit <icbtworkfor@gmail.com>
2025-06-12 14:17:30 +0530

    Update DatabaseHelper.cs

--- DatabaseHelper.cs
+++ DatabaseHelper.cs
@@ -1,11 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Configuration; 
+using System.Data.SqlClient;
 
 namespace LibraryApp
 {
     public class DatabaseHelper
     {
+        public static string GetConnectionString()
+        {
+          
+            return ConfigurationManager.ConnectionStrings["LibraryDbConnection"].ConnectionString;
+        }
+
+        public static SqlConnection GetConnection()   
+        {
+            return new SqlConnection(GetConnectionString());  
+        }
     }
 }
